3246d7c3c60285212db75a8c0b683134228ed051
Ethan Seow <eseow6088@bths.edu>
2019-02-02 19:06:20 -0500

    Drive the robot with smooth curve

--- src/main/java/frc/robot/commands/DriveCommand.java
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -1,10 +1,15 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
-import frc.robot.OI;
 import frc.robot.Robot;
 
 public class DriveCommand extends Command {
+
+    double lastJoystickLeft;
+    double lastJoystickRight;
+    double leftSpeed;
+    double rightSpeed;
+
     public DriveCommand() {
         requires(Robot.sDrive);
     }
@@ -16,14 +21,28 @@ public class DriveCommand extends Command {
 
     @Override
     protected void execute() {
-        Robot.sDrive.setLeft(OI.leftJoystick.getY());
-        Robot.sDrive.setRight(OI.rightJoystick.getY());
+        if ((Robot.m_oi.getLeftJoystick().getY() < 0 && lastJoystickLeft > 0) || (Robot.m_oi.getLeftJoystick().getY() > 0 && lastJoystickLeft < 0)){
+            leftSpeed = 0;
+        }else{
+            leftSpeed = Robot.m_oi.getLeftJoystick().getY();
+        }
+        if ((Robot.m_oi.getRightJoystick().getY() < 0 && lastJoystickRight > 0) || (Robot.m_oi.getRightJoystick().getY() > 0 && lastJoystickRight < 0)){
+            rightSpeed = 0;
+        }
+        else{
+            rightSpeed = Robot.m_oi.getRightJoystick().getY();
+        }
+
+        Robot.sDrive.setLeft(leftSpeed);
+        Robot.sDrive.setRight(rightSpeed);
+
+
+        lastJoystickLeft = Robot.m_oi.getLeftJoystick().getY();
+        lastJoystickRight = Robot.m_oi.getRightJoystick().getY();
     }
 
     @Override
-    protected boolean isFinished() {
-    return false;
-    }
+    protected boolean isFinished() { return false; }
 
     @Override
     protected void end() {
@@ -36,4 +55,5 @@ public class DriveCommand extends Command {
         System.out.println("STOPPED TANK DRIVE");
         Robot.sDrive.stop();
     }
+
 }
