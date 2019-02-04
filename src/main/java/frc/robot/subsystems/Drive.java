efdd0bbf92493436c28057b2f38722a10d391f81
Ethan Seow <eseow6088@bths.edu>
2019-02-04 16:28:30 -0500

    Format the Drivetrain

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -4,13 +4,12 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
-import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.RobotMap;
 import frc.robot.commands.DriveCommand;
 
-import com.revrobotics.CANSparkMax;
-
 public class Drive extends Subsystem {
+
     CANSparkMax leftTop;
     CANSparkMax leftMid;
     CANSparkMax leftBot;
@@ -20,30 +19,38 @@ public class Drive extends Subsystem {
 
     SpeedControllerGroup left;
     SpeedControllerGroup right;
+    
     public Drive(){
+        leftTop = new CANSparkMax(RobotMap.LEFT_TOP_MOTOR, MotorType.kBrushless);
+        leftMid = new CANSparkMax(RobotMap.LEFT_MID_MOTOR, MotorType.kBrushless);
+        leftBot = new CANSparkMax(RobotMap.LEFT_BOT_MOTOR, MotorType.kBrushless);
 
-        leftTop = new CANSparkMax(0, MotorType.kBrushless);
-        leftMid = new CANSparkMax(1, MotorType.kBrushless);
-        leftBot = new CANSparkMax(2, MotorType.kBrushless);
-        rightTop = new CANSparkMax(3, MotorType.kBrushless);
-        rightMid = new CANSparkMax(4, MotorType.kBrushless);
-        leftBot = new CANSparkMax(5, MotorType.kBrushless);
+        rightTop = new CANSparkMax(RobotMap.RIGHT_TOP_MOTOR, MotorType.kBrushless);
+        rightMid = new CANSparkMax(RobotMap.RIGHT_MID_MOTOR, MotorType.kBrushless);
+        leftBot = new CANSparkMax(RobotMap.RIGHT_BOT_MOTOR, MotorType.kBrushless);
 
         left = new SpeedControllerGroup(leftTop, leftMid,leftBot);
         right = new SpeedControllerGroup(rightTop,rightMid,rightBot);        
     }
+
     @Override
     public void initDefaultCommand() {
         new DriveCommand();
     }
+
     public void setLeft(double speed){
+        // Sets the speed to the speed parameter.
         left.set(speed);
     }
+
     public void setRight(double speed){
+        // Sets the speed to the speed parameter.
         right.set(speed);
     }
+
     public void stop(){
         left.set(0);
         right.set(0);
     }
+
 }
