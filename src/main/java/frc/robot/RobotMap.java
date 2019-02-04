0fc670881e3cf4ea94f5ca0e07968c983636415d
Ethan Seow <eseow6088@bths.edu>
2019-02-04 16:24:05 -0500

    Map the robot with the port and channel values

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,6 +1,7 @@
 package frc.robot;
 
 public class RobotMap {
+    
     public static int LEFT_TOP_MOTOR = 0;
     public static int LEFT_MID_MOTOR = 1;
     public static int LEFT_BOT_MOTOR = 2;
@@ -8,4 +9,8 @@ public class RobotMap {
     public static int RIGHT_TOP_MOTOR = 3;
     public static int RIGHT_MID_MOTOR = 4;
     public static int RIGHT_BOT_MOTOR = 5;
+
+    public static int LEFT_JOYSTICK = 0;
+    public static int RIGHT_JOYSTICK = 1;
+
 }
