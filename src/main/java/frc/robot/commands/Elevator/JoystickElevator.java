242c7b9a78f907d190ac7e7e6d27f50c2b4be2c6
unknown <ssugrim1430@bths.edu>
2019-02-07 16:45:31 -0500

    add the JoystickElevator command

---
+++ src/main/java/frc/robot/commands/Elevator/JoystickElevator.java
@@ -0,0 +1,27 @@
+package frc.robot.commands.Elevator;
+
+import frc.robot.OI;
+import frc.robot.Robot;
+import edu.wpi.first.wpilibj.command.Command;
+
+public class JoystickElevator extends Command {
+
+    public JoystickElevator() { requires(Robot.sElevator); }
+
+    @Override
+    protected void initialize() { }
+
+    @Override
+    protected void execute() {
+        Robot.sElevator.setMotors(OI.operator.getY());
+    }
+
+    @Override
+    protected boolean isFinished() { return false; }
+
+    @Override
+    protected void end() { }
+
+    @Override
+    protected void interrupted() { }
+}
