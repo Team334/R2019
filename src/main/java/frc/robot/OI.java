fa94458d364309a16007d22d01f4a7c63ed07d04
Shashi Sugrim <34145834+ShashiSugrim@users.noreply.github.com>
2019-02-07 17:07:09 -0500

    Add elevator subsystem and commands (#3)
    * add basic Elevator
    
    * add Encoder to Elevator
    
    * Adding elevator commands
    
    * Refactor code
    
    * Fixing robot imports
    
    * add Elevator collapse command
    
    * fix placeholders
    
    * fix placeholder values
    
    * add the code for limit switches
    
    * make necessary convention changes
    
    * add the JoystickElevator command
    
    * fix the spacing conventions across files
    
    * fix the spacing conventions for classes
    
    * fix the default Elevator command

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -1,5 +1,30 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import frc.robot.commands.Elevator.*;
+import frc.robot.commands.*;
+
 public class OI {
     
+    // Make joystick.
+    public static Joystick operator;
+
+    private Button elevatorLow;
+    private Button elevatorMed;
+    private Button elevatorHigh;
+
+    public OI() {
+        // Initialize buttons.
+        elevatorLow = new JoystickButton(operator, RobotMap.ELEVATOR_BUTTON_LOW);
+        elevatorMed = new JoystickButton(operator, RobotMap.ELEVATOR_BUTTON_MED);
+        elevatorHigh = new JoystickButton(operator, RobotMap.ELEVATOR_BUTTON_HIGH);
+
+        // Make button actions.
+        elevatorLow.whenPressed(new SetElevatorHeight(Constants.ELEVATOR_ENC_LOW));
+        elevatorMed.whenPressed(new SetElevatorHeight(Constants.ELEVATOR_ENC_MED));
+        elevatorHigh.whenPressed(new SetElevatorHeight(Constants.ELEVATOR_ENC_HIGH));
+    }
+
 }
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,50 +1,39 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import frc.robot.subsystems.*;
 
 public class Robot extends TimedRobot {
+
+    public static Elevator sElevator;
+
     @Override
     public void robotInit() {
-
+        sElevator = new Elevator();
     }
 
     @Override
-    public void robotPeriodic() {
+    public void robotPeriodic() { }
+
+    @Override
+    public void disabledInit() { }
+
+    @Override
+    public void disabledPeriodic() { }
+
+    @Override
+    public void autonomousInit() { }
+
+    @Override
+    public void autonomousPeriodic() { }
+
+    @Override
+    public void teleopInit() { }
+
+    @Override
+    public void teleopPeriodic() { }
+
+    @Override
+    public void testPeriodic() { }
     
-    }
-
-    @Override
-    public void disabledInit() {
-    
-    }
-
-    @Override
-    public void disabledPeriodic() {
-
-    }
-
-    @Override
-    public void autonomousInit() {
-
-    }
-
-    @Override
-    public void autonomousPeriodic() {
-        
-    }
-
-    @Override
-    public void teleopInit() {
-
-    }
-
-    @Override
-    public void teleopPeriodic() {
-    
-    }
-
-    @Override
-    public void testPeriodic() {
-
-    }
 }
