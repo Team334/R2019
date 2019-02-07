920ee28b55de45821eed621d6b2ef93000ca6949
ethanseow <42679013+ethanseow@users.noreply.github.com>
2019-02-07 16:20:29 -0500

    Update RobotMap.java

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -13,7 +13,7 @@ public class RobotMap {
     public static int LEFT_JOYSTICK = 0;
     public static int RIGHT_JOYSTICK = 1;
 
-    public static int LEFT_DRIVE_ENCODER_A = 0;
-    public static int RIGHT_DRIVE_ENCODER_B = 1;
+    public static int LEFT_DRIVE_ENCODER_A = -1;
+    public static int RIGHT_DRIVE_ENCODER_B = -1;
     
 }
