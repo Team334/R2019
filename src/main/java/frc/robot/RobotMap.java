40da1ddd7fbcde6bda61d61f8518537ae11725a7
ethanseow <42679013+ethanseow@users.noreply.github.com>
2019-02-07 16:13:18 -0500

    Update RobotMap.java

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,17 +1,17 @@
 package frc.robot;
 
 public class RobotMap {
-    public static int LEFT_TOP_MOTOR = 0;
-    public static int LEFT_MID_MOTOR = 1;
-    public static int LEFT_BOT_MOTOR = 2;
+    public static int LEFT_TOP_DRIVE_MOTOR = 0;
+    public static int LEFT_MID_DRIVE_MOTOR = 1;
+    public static int LEFT_BOT_DRIVE_MOTOR = 2;
 
-    public static int RIGHT_TOP_MOTOR = 3;
-    public static int RIGHT_MID_MOTOR = 4;
-    public static int RIGHT_BOT_MOTOR = 5;
+    public static int RIGHT_TOP_DRIVE_MOTOR = 3;
+    public static int RIGHT_MID_DRIVE_MOTOR = 4;
+    public static int RIGHT_BOT_DRIVE_MOTOR = 5;
 
     public static int LEFT_JOYSTICK = 0;
     public static int RIGHT_JOYSTICK = 1;
 
-    public static int LEFT_ENCODER = 0;
-    public static int RIGHT_ENCODER = 1;
+    public static int LEFT_DRIVE_ENCODER_A = 0;
+    public static int RIGHT_DRIVE_ENCODER_B = 1;
 }
