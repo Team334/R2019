eda07c5ba3c4587f88cec7be4483b018a0d78f79
apanas9246 <35401522+apanas9246@users.noreply.github.com>
2019-02-09 11:45:12 -0500

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -9,17 +9,17 @@ public class Constants {
     public static final double PROPUP_MOTOR_SPEED_MULT = 0.8;
 
     // Intake Potentiometer PID values and setpoint constants.
-    public static final double INTAKE_PID_P = 0.05;
-    public static final double INTAKE_PID_I = 0;
-    public static final double INTAKE_PID_D = 0.07;
+    public static final double INTAKE_PID_P = -1;
+    public static final double INTAKE_PID_I = -1;
+    public static final double INTAKE_PID_D = -1;
 
-    public static final double HOLD_BALL_SETPOINT = 100;
-    public static final double CLOSE_ARMS_SETPOINT = 1000;
-    public static final double LATCH_HATCH_SETPOINT = 2000;
+    public static final double HOLD_BALL_SETPOINT = -1;
+    public static final double CLOSE_ARMS_SETPOINT = -1;
+    public static final double LATCH_HATCH_SETPOINT = -1;
 
-    public static final double INTAKE_PID_TOLERANCE = 1;
-    public static final double INTAKE_PID_RANGE_LOWER = -0.5;
-    public static final double INTAKE_PID_RANGE_UPPER = 0.5;
+    public static final double INTAKE_PID_TOLERANCE = -1;
+    public static final double INTAKE_PID_RANGE_LOWER = -1;
+    public static final double INTAKE_PID_RANGE_UPPER = -1;
     
     public static final double ELEVATOR_ENC_P = -1;
     public static final double ELEVATOR_ENC_I = -1;
