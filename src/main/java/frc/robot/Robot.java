524b345acf3e0690cc35fbe9eb3a4ce91e5105b7
Ethan Seow <eseow6088@bths.edu>
2019-02-17 13:01:31 -0500

    modified:   src/main/java/frc/robot/Robot.java 	modified:   src/main/java/frc/robot/RobotMap.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,7 +2,6 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.subsystems.*;
 
 public class Robot extends TimedRobot {
--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -36,20 +36,21 @@ public class RobotMap {
 
     public static final int ELEVATOR_ENC_A = -1;
     public static final int ELEVATOR_ENC_B = -1;
-    
+
     // Drivetrain.
-    public static int LEFT_A_DRIVE_MOTOR = 3;
-    public static int LEFT_B_DRIVE_MOTOR = 4;
-    public static int LEFT_C_DRIVE_MOTOR = 5;
+    public static final int LEFT_A_DRIVE_MOTOR = 3;
+    public static final int LEFT_B_DRIVE_MOTOR = 4;
+    public static final int LEFT_C_DRIVE_MOTOR = 5;
 
-    public static int RIGHT_A_DRIVE_MOTOR = 0;
-    public static int RIGHT_B_DRIVE_MOTOR = 1;
-    public static int RIGHT_C_DRIVE_MOTOR = 2;
+    public static final int RIGHT_A_DRIVE_MOTOR = 0;
+    public static final int RIGHT_B_DRIVE_MOTOR = 1;
+    public static final int RIGHT_C_DRIVE_MOTOR = 2;
   
-    public static int LEFT_JOYSTICK = 0;
-    public static int RIGHT_JOYSTICK = 1;
 
-    public static int LEFT_DRIVE_ENCODER_A = -1;
-    public static int RIGHT_DRIVE_ENCODER_B = -1;
+    public static final int LEFT_JOYSTICK = 0;
+    public static final int RIGHT_JOYSTICK = 1;
+
+    public static final int LEFT_DRIVE_ENCODER_A = -1;
+    public static final int RIGHT_DRIVE_ENCODER_B = -1;
     
 }
