38fc7aff47a357023a1a571c72e5bb8ba58b2e3c
ethanseow <42679013+ethanseow@users.noreply.github.com>
2019-02-17 13:43:22 -0500

    Drivetrain (#6)
    * map all the joysticks
    
    * Map the joystick
    
    * Drive the robot with smooth curve
    
    * Map the joystick
    
    * Drive the robot
    
    * Control the robot code
    
    * Map the robot with the port and channel values
    
    * Format the Drivetrain
    
    * Map the joysticks
    
    * 	modified:   src/main/java/frc/robot/OI.java
    	modified:   src/main/java/frc/robot/Robot.java
    	modified:   src/main/java/frc/robot/commands/DriveCommand.java
    
    * Update RobotMap.java
    
    * Update Drive.java
    
    * Update OI.java
    
    * Update RobotMap.java
    
    * Update RobotMap.java
    
    * Update Drive.java
    
    * Update Drive.java
    
    * 	modified:   src/main/java/frc/robot/subsystems/Drive.java
    
    * 	modified:   src/main/java/frc/robot/OI.java
    
    * 	modified:   src/main/java/frc/robot/OI.java
    
    * 	src/main/java/frc/robot/commands/Drivetrain/
    
    * 	src/main/java/frc/robot/commands/Drivetrain/
    
    * 	new file:   src/main/java/frc/robot/commands/Drivetrain/DriveCommand.java
    
    * 	modified:   src/main/java/frc/robot/Robot.java
    	modified:   src/main/java/frc/robot/commands/Drivetrain/DriveCommand.java
    
    * Update DriveCommand.java
    
    * Update RobotMap.java
    
    * Update Drive.java
    
    * 	modified:   src/main/java/frc/robot/Robot.java
    	modified:   src/main/java/frc/robot/RobotMap.java
    
    * Update Drive.java
    
    * Update Drive.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,11 +2,10 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.subsystems.*;
 
 public class Robot extends TimedRobot {
-
+  
     public static Intake sIntake;
     public static Elevator sElevator;
     public static Drive sDrive;
