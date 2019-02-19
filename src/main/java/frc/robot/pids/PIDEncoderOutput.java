ca701409b72612e7ae0c79a798e9566c0ac233d1
ethanseow <42679013+ethanseow@users.noreply.github.com>
2019-02-19 10:18:40 -0500

    Update PIDEncoderOutput.java

---
+++ src/main/java/frc/robot/pids/PIDEncoderOutput.java
@@ -0,0 +1,10 @@
+package frc.robot.pids;
+
+import edu.wpi.first.wpilibj.PIDOutput;
+
+public class PIDEncoderOutput implements PIDOutput {
+
+    @Override
+    public void pidWrite(double output) { }
+
+}
